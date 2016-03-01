8219acfea7ac4cb6a5469fcab06bd079f3406ec6
unknown <jonathan.orro@gmail.com>
2016-03-01 11:00:00 +0100

    Changed to tensorflow 0.7.0 in Dockerfile.

--- docker/run_chatbot.sh
+++ docker/run_chatbot.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker build -t joniktor/chatbot:gpu -f ./docker/Dockerfile . 
+#docker build -t joniktor/chatbot:gpu -f ./docker/Dockerfile . 
 
 ./docker/docker_run_gpu.sh -v /home/joniktor/examples/chatbot1.0/:/root/ --rm -t joniktor/chatbot:gpu
