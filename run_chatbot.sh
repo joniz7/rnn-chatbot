b7b3467d151c614a2d468309fbd1f60175257742
unknown <jonathan.orro@gmail.com>
2016-03-01 11:31:42 +0100

    More changes to job scripts

--- run_chatbot.sh
+++ run_chatbot.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-
+echo "now run"
 project_run=$(basename "$0")
 
 #create log dir
 mkdir log 
 
 submit job
+
+echo "submit"
 qsub -cwd \
   -e ./log/$project_run.error \
   -o ./log/$project_run.log \
--- src/run_chatbot.sh
+++ src/run_chatbot.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-
+echo "source!!"
 python -u ./src/translate.py --vocab_size=30000 --size=1024 --num_layers=3 --batch_size=64 --max_running_time=40 --embedding_dimensions=300
