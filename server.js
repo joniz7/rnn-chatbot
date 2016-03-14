2889b9603dd464fdbf6cd66906bfe9c8a48d0d7c
unknown <jonathan.orro@gmail.com>
2016-03-14 16:28:03 +0100

    Changed IP

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(bodyParser.json());
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://192.168.99.100');
+    res.setHeader('Access-Control-Allow-Origin', 'http://128.199.46.170:32770');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
