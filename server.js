978cbda48fc4ad2ea19d80a68e2464d19c68af28
unknown <jonathan.orro@gmail.com>
2016-03-14 16:33:24 +0100

    Changed port

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(bodyParser.json());
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://128.199.46.170:32770');
+    res.setHeader('Access-Control-Allow-Origin', 'http://128.199.46.170:59254');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
