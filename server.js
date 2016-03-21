ace8da56bf75aeafef5ba6881e23e4d35cbdbb62
unknown <jonathan.orro@gmail.com>
2016-03-21 12:24:45 +0100

    Changed access port

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(bodyParser.json());
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://128.199.46.170:59254');
+    res.setHeader('Access-Control-Allow-Origin', 'http://128.199.46.170:27241');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
