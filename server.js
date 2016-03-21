fde1d7b5bb9b2e8360ac6908b665ed19160fd556
unknown <jonathan.orro@gmail.com>
2016-03-21 11:11:18 +0100

    Fixed bug

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.set("jsonp callback name", "cb");
 
 var time = new Date();
 
-var filepath = 'data/chatlogs/log'+time.getTime()+'.txt';
+var filepath = '../data/chatlogs/log'+time.getTime()+'.txt';
 fs.openSync(filepath, "w");
 
 function writeFile(msg) {
