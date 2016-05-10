d526e3d088453e3b0e0a8c9d9671956f4d45c8e9
Jonathan Orrö <jonathan.orro@gmail.com>
2016-05-10 09:24:58 +0200

    Changed runscript

--- src/run_chatbot.sh
+++ src/run_chatbot.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 EXP_PATH=/home/joniktor/experiments/09-05-2016/rnn-chatbot
 
-export CUDA_VISIBLE_DEVICES=0
-./src/run_one_chatbot.sh 1 python -u ./src/translate.py --vocab_size=90000 --size=1800 --num_layers=2 --use_lstm=True --batch_size=64 --max_running_time=40 --embedding_dimensions=300 --checkpoint_dir="../checkpoints/1" --summary_path="../data/summaries/1" >> $EXP_PATH/log/log1.txt &
-export CUDA_VISIBLE_DEVICES=0
-./src/run_one_chatbot.sh 2 python -u ./src/translate.py --save_states=False --vocab_size=90000 --size=1800 --num_layers=2 --use_lstm=True --batch_size=64 --max_running_time=55 --embedding_dimensions=300 --checkpoint_dir="../checkpoints/2" --summary_path="../data/summaries/2" >> $EXP_PATH/log/log2.txt
+#export CUDA_VISIBLE_DEVICES=0
+./src/run_one_chatbot.sh 1 python -u ./src/translate.py --vocab_size=90000 --size=1800 --num_layers=2 --use_lstm=True --batch_size=64 --max_running_time=40 --embedding_dimensions=300 --checkpoint_dir="../checkpoints/1" --summary_path="../data/summaries/1" >> $EXP_PATH/log/log1.txt
+#export CUDA_VISIBLE_DEVICES=1
+#./src/run_one_chatbot.sh 2 python -u ./src/translate.py --save_states=False --vocab_size=90000 --size=1800 --num_layers=2 --use_lstm=True --batch_size=64 --max_running_time=50 --embedding_dimensions=300 --checkpoint_dir="../checkpoints/2" --summary_path="../data/summaries/2" >> $EXP_PATH/log/log2.txt
 #export CUDA_VISIBLE_DEVICES=2
 #./src/run_one_chatbot.sh 3 python -u ./src/translate.py --quest_drop_rate=0.8 --excl_drop_rate=0.8 --period_drop_rate=0.8 --comma_drop_rate=0.8 --dots_drop_rate=0.8 --vocab_size=90000 --size=1800 --num_layers=2 --use_lstm=True --batch_size=64 --max_running_time=40 --embedding_dimensions=300 --checkpoint_dir="../checkpoints/3" --summary_path="../data/summaries/3" >> $EXP_PATH/log/log3.txt
 #export CUDA_VISIBLE_DEVICES=3
